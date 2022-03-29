b4fa73b83e2d7f09f683aca62f9a2e10f2dbe530
caroldmg0905 <99288502+caroldmg0905@users.noreply.github.com>
2022-03-29 16:02:20 -0300

    Add files via upload

---
+++ Comentário.java
@@ -0,0 +1,8 @@
+public class Comentário {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!\n" + "Laboratório de Programação 2 - LP2\n" + "2 ano Informática\n" + "Seja bem vindo!\n" + "Vamos aprender Java.");
+        
+    }
+}
+
